fa64fdd6910e410a31b12abc4d340b3725509f37
William <151616274+Rwandraal@users.noreply.github.com>
2024-06-08 11:21:13 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@ session_start();
     <title>Document</title>
 </head>
 
-<body>
+<body id="body" style="background-color: #FFF5F7;">
     <section class="py-5 text-center container">
         <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
